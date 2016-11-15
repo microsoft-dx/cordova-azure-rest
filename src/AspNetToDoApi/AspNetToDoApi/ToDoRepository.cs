bd03aca470f3eb2967936329191e21c076f09b4f
Radu Matei <matei.radu94@gmail.com>
2016-11-15 13:51:16 +0000

    Added in-memory ToDoRepository

---
+++ src/AspNetToDoApi/AspNetToDoApi/ToDoRepository.cs
@@ -0,0 +1,51 @@
+﻿using AspNetToDoApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AspNetToDoApi
+{
+    public class ToDoRepository
+    {
+        private List<ToDoItem> _toDoList { get; set; }
+
+        public ToDoRepository()
+        {
+            _toDoList = new List<ToDoItem>()
+            {
+                new ToDoItem(1, "Feed the dog", "Remember to feed the dog", false),
+                new ToDoItem(2, "Watch Star Wars", "Remember to watch the new Star Wars movie", true)
+            };
+        }
+
+        public List<ToDoItem> GetToDoItems()
+        {
+            return _toDoList;
+        }
+
+        public void CreateToDo(ToDoItem toDo)
+        {
+            _toDoList.Add(toDo);
+        }
+
+        public void Delete(int id)
+        {
+            var toDo = _toDoList.FirstOrDefault(t => t.Id == id);
+
+            if (toDo != null)
+                _toDoList.Remove(toDo);
+        }
+
+        public void Update(ToDoItem toDo)
+        {
+            var toDoToUpdate = _toDoList.FirstOrDefault(t => t.Id == toDo.Id);
+
+            if (toDoToUpdate != null)
+                toDoToUpdate.UpdateToDo(toDo);
+        }
+
+        public ToDoItem GetById(int id)
+        {
+            return _toDoList.FirstOrDefault(t => t.Id == id);
+        }
+    }
+}
