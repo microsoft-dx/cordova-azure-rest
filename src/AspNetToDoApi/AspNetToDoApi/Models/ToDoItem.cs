c906e5baaa460a0b1cbd061edcbd02962681188d
Radu Matei <matei.radu94@gmail.com>
2016-11-15 13:50:57 +0000

    Added ToDoItem model

---
+++ src/AspNetToDoApi/AspNetToDoApi/Models/ToDoItem.cs
@@ -0,0 +1,30 @@
+﻿namespace AspNetToDoApi.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool Done { get; set; }
+
+        public ToDoItem()
+        {
+        }
+
+        public ToDoItem(int id, string name, string notes, bool done)
+        {
+            Id = id;
+            Name = name;
+            Notes = notes;
+            Done = done;
+        }
+
+        public void UpdateToDo(ToDoItem toDo)
+        {
+            Id = toDo.Id;
+            Name = toDo.Name;
+            Notes = toDo.Notes;
+            Done = toDo.Done;
+        }
+    }
+}
