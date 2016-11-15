28bcf9ae2d4bf625b6d6ae9148778fffb01fdf02
Radu Matei <matei.radu94@gmail.com>
2016-11-15 13:51:30 +0000

    Added ToDoController

---
+++ src/AspNetToDoApi/AspNetToDoApi/Controllers/ToDoController.cs
@@ -0,0 +1,41 @@
+﻿using AspNetToDoApi.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace AspNetToDoApi.Controllers
+{
+    public class ToDoController : ApiController
+    {
+        private static ToDoRepository _toDoRepository = new ToDoRepository();
+        
+         [HttpGet]
+         public List<ToDoItem> GetToDoItems()
+        {
+            return _toDoRepository.GetToDoItems();
+        }
+
+        [HttpGet]
+        public ToDoItem GetToDoItemById(int id)
+        {
+            return _toDoRepository.GetById(id);
+        }
+
+        [HttpPost]
+        public void CreateToDoItem(ToDoItem toDo)
+        {
+            _toDoRepository.CreateToDo(toDo);
+        }
+
+        [HttpPost]
+        public void UpdateToDoItem(ToDoItem toDo)
+        {
+            _toDoRepository.Update(toDo);
+        }
+
+        [HttpDelete]
+        public void DeleteToDoItem(int id)
+        {
+            _toDoRepository.Delete(id);
+        }
+    }
+}
