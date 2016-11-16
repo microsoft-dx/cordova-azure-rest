3853735cc8c926d611785bfb3d6ceafb1fd8b0f5
Radu Matei <matei.radu94@gmail.com>
2016-11-16 17:24:55 +0200

    Added basic Spring project structure

---
+++ src/JavaToDoApi/src/main/java/com/fils/JavaToDoApiApplication.java
@@ -0,0 +1,12 @@
+package com.fils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaToDoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaToDoApiApplication.class, args);
+	}
+}
